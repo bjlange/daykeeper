4fe02f8c8a4f93e0d2d6675d9aef3fb89a40bfa6
Brian Lange <thebrianlange@gmail.com>
2013-03-13 12:27:41 -0500

    Add render function for limbo. Closes #9

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ Parse.initialize("Ma2oFlJurgeYxRRkoBVga7MfMLGgOlpHxARED2VR", "j3CjT3KB4G9gdWYema
 var Task = Parse.Object.extend("TasksObject");
 var Todos;
 
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
+
 function initialize() {
     // attach a listener for push transitions, and run necessary render functions
     window.addEventListener('push', masterRender);
@@ -15,13 +22,74 @@ window.onload = initialize;
 function masterRender() {
     var page = window.location.pathname.replace('/','');
     var render_map={'agenda.html':renderAgenda,
-		    'unscheduled.html':renderUnscheduledList};
+		    'unscheduled.html':renderUnscheduledList,
+		    'limbo.html':renderLimbo};
 
     render_map[page]();
 }
 
 function renderAgenda() {
     console.log('hullo');
+}
+
+function renderLimbo() {
+    
+    var now = new Date()
+    console.log(now);
+    var query = new Parse.Query(Task);
+    
+    query.lessThan("date",now);
+
+    query.find({
+	success : function(results) {
+	    Todos = results;
+	    
+	    // remove "ongoing" tasks- check the durations
+	    for(var j=0; j<Todos.length; j++) {
+		var dur = Todos[j].get("duration");
+		var endtime = new Date(Todos[j].get("date").getTime() 
+				       + dur*60000);
+		if(endtime > now) {
+		    //console.log('Still working on', Todos[j].get('Title'));
+		    Todos.remove(j);
+		}
+		
+	    }
+	    
+	    var ul = document.getElementById("limboList");
+	    
+	    // clear out the list
+	    ul.innerHTML = "";
+
+	    // load all tasks
+	    for(var i=0; i<Todos.length; i++) {
+		var title = Todos[i].get("Title");
+		var li = document.createElement('li');
+		var cb = document.createElement('input');
+		cb.setAttribute("type","checkbox");
+		var a = document.createElement('a');
+		var span = document.createElement('span');
+		span.setAttribute("class", "chevron");
+		a.href = "detailview.html";
+		a.title = title
+		a.setAttribute("data-transition", "slide-in");
+		a.setAttribute("style","display:inline;");
+		a.appendChild(document.createTextNode(title));
+		li.appendChild(cb);
+		li.appendChild(a);
+		li.appendChild(span);
+		ul.appendChild(li);
+		
+		//.innerHTML = "<li><input type=\"checkbox\" /> <a href=\"task_detail.html\* data-transition=\"slide-in\" style=\"display:inline;\">"+title+"</a><span class=\"chevron\"></li>";
+	    }
+	},
+	error : function(error) {
+	    alert("ERROR!");
+	}
+    });
+    
+    
+
 }
 
 function renderUnscheduledList() {
